3bffe6e579891a294ac98a98cb7ca85aa7c72cc7
Yash Maini <120708372+Yashmaini30@users.noreply.github.com>
2025-01-04 19:20:28 +0530

    optimized using maps

--- 1930. Unique Length-3 Palindromic Subsequences.java
+++ 1930. Unique Length-3 Palindromic Subsequences.java
@@ -1,3 +1,4 @@
+//set
 class Solution {
     public int countPalindromicSubsequence(String s) {
         HashSet<Character> set = new HashSet<>();
@@ -17,6 +18,34 @@ class Solution {
                     last = i;
                 }
             }
+            if(first == last) continue;
+            HashSet<Character> set1 = new HashSet<>();
+            for(int i=first+1;i<last;i++){
+                set1.add(s.charAt(i));
+            }
+            count += set1.size();
+        }
+        return count;
+    }
+}
+//map
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        HashMap<Character,Integer> firstOcc = new HashMap<Character,Integer>();
+        HashMap<Character,Integer> lastOcc = new HashMap<Character,Integer>();
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(!firstOcc.containsKey(ch)){
+                firstOcc.put(ch,i);
+            }
+            lastOcc.put(ch,i);
+        }
+
+        int count=0;
+        for(char ch : firstOcc.keySet()){
+            int first=firstOcc.get(ch);
+            int last = lastOcc.get(ch);
             if(first == last) continue;
             HashSet<Character> set1 = new HashSet<>();
             for(int i=first+1;i<last;i++){
