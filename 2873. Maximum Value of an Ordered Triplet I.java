a711a40d10520d11e28a1c3948906ffb153d5e58
Yash Maini <120708372+Yashmaini30@users.noreply.github.com>
2025-04-02 21:48:44 +0530

    optimized

--- 2873. Maximum Value of an Ordered Triplet I.java
+++ 2873. Maximum Value of an Ordered Triplet I.java
@@ -1,5 +1,16 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
+        long maxTriplet = 0, maxElement = 0, maxDiff = 0;
+        for (int num : nums) {
+            maxTriplet = Math.max(maxTriplet, maxDiff * num);
+            maxDiff = Math.max(maxDiff, maxElement - num);
+            maxElement = Math.max(maxElement, num);
+        }
+        return maxTriplet;
+    }
+}
+/*class Solution {
+    public long maximumTripletValue(int[] nums) {
         long maxTriplet = 0;
         for (int i = 0; i < nums.length; i++) {
             for (int k = nums.length - 1; k > i; k--) {
@@ -12,4 +23,4 @@ class Solution {
         }
         return Math.max(0, maxTriplet);
     }
-}
+}*/
