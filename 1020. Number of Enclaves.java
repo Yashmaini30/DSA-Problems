19e255a20094f5e6f26810e13c3523feefbf2a25
Yash Maini <120708372+Yashmaini30@users.noreply.github.com>
2025-04-26 21:32:27 +0530

    solved

---
+++ 1020. Number of Enclaves.java
@@ -0,0 +1,31 @@
+class Solution { 
+    public int numEnclaves(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 1) dfs(grid, i, 0);
+            if (grid[i][n - 1] == 1) dfs(grid, i, n - 1);
+        }
+        for (int j = 0; j < n; j++) {
+            if (grid[0][j] == 1) dfs(grid, 0, j);
+            if (grid[m - 1][j] == 1) dfs(grid, m - 1, j);
+        }
+
+        int count = 0;A
+        for (int[] row : grid) {
+            for (int cell : row) {
+                if (cell == 1) count++;
+            }
+        }
+        return count;
+    }
+    
+    private void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) return;
+        grid[i][j] = 0;
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
