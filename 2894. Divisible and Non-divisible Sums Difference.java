14fe41be89ed32cc8263cd2037b018cc22aafaa2
Yash Maini <120708372+Yashmaini30@users.noreply.github.com>
2025-05-27 23:09:44 +0530

    solved using formula

--- 2894. Divisible and Non-divisible Sums Difference.java
+++ 2894. Divisible and Non-divisible Sums Difference.java
@@ -1,11 +1,17 @@
 class Solution {
     public int differenceOfSums(int n, int m) {
-        int num1 = 0, num2 = 0;
-        for (int i=1;i<=n;i++){
-            if(i%m == 0) num2 +=i;
-            else num1 += i;
-        }
-        return num1 - num2;
+        // int num1 = 0, num2 = 0;
+        // for (int i=1;i<=n;i++){
+        //     if(i%m == 0) num2 +=i;
+        //     else num1 += i;
+        // }
+        // return num1 - num2;
+
+        int sum = n*(n+1)/2;
+        int div = n/m;
+        int divSum = m*div*(div+1)/2;
+
+        return sum - 2*divSum;
         
     }
 }
