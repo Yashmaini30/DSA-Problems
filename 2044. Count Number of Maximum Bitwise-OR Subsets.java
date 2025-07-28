96b950d69a6d4e213398bc30cde20e6b41d748cc
Yash Maini <120708372+Yashmaini30@users.noreply.github.com>
2025-07-28 19:09:27 +0530

    solved

---
+++ 2044. Count Number of Maximum Bitwise-OR Subsets.java
@@ -0,0 +1,26 @@
+public class Solution {
+    private int maxOr = 0;
+    private int count = 0;
+
+    public int countMaxOrSubsets(int[] nums) {
+        maxOr = 0;
+        for (int num : nums) {
+            maxOr |= num; 
+        }
+        dfs(nums, 0, 0);
+        return count;
+    }
+
+    private void dfs(int[] nums, int index, int currentOr) {
+        if (index == nums.length) {
+            if (currentOr == maxOr) {
+                count++;
+            }
+            return;
+        }
+        if (currentOr > maxOr) return;
+
+        dfs(nums, index + 1, currentOr | nums[index]);
+        dfs(nums, index + 1, currentOr);
+    }
+}
