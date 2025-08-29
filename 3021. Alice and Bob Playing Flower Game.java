768a08ba2d7ace1edfb2cc8c5a3bc085abdfee78
Yash Maini <120708372+Yashmaini30@users.noreply.github.com>
2025-08-29 11:36:37 +0530

    solved

---
+++ 3021. Alice and Bob Playing Flower Game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+        
+        // int res = 0;
+        // for (int i=1;i<=m;i++){
+        //     for (int j=1;j<=n;j++){
+        //         if((i+j)%2 !=0){
+        //             res++;
+        //         }
+        //     }
+        // }
+        // return res;
+
+        // Half cases par alice Half par Bob
+
+        return (long) m*n/2;
+    }
+}
