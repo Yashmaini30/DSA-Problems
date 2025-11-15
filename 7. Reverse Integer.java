b3d343066922e4118390d81d166afa840bc2e853
Yash Maini <120708372+Yashmaini30@users.noreply.github.com>
2025-11-15 18:08:26 +0530

    Implement reverse integer function

---
+++ 7. Reverse Integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+
+        int rev = 0;
+        while(x != 0){
+            int d = x % 10;
+            x /= 10;
+
+            if (rev > 214748364) return 0;
+            if (rev < -214748364) return 0;
+
+            rev = rev * 10 + d;
+        }
+        return rev;
+    }
+}
